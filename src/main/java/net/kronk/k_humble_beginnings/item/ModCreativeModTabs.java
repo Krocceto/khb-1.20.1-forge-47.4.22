24454551cc140e2f0ecc2bad3b9cbc5b21b858ad
krocc <krocceto@gmail.com>
2026-08-11 20:53:53 -0500

    :P

--- src/main/java/net/kronk/k_humble_beginnings/item/ModCreativeModTabs.java
+++ src/main/java/net/kronk/k_humble_beginnings/item/ModCreativeModTabs.java
@@ -10,18 +10,46 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeModTabs {
+
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
-            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, HumbleBeginnings.MOD_ID);
+        DeferredRegister.create(
+            Registries.CREATIVE_MODE_TAB,
+            HumbleBeginnings.MOD_ID
+        );
 
-    public static final RegistryObject<CreativeModeTab> HB_TAB = CREATIVE_MODE_TABS.register("hb_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.PILLOW.get()))
-                    .title(Component.translatable("creativetab.hb_tab"))
-                    .displayItems((pParameters, pOutput) -> {
-                        pOutput.accept(ModItems.EXAMPLE_ITEM.get());
-                        pOutput.accept(ModItems.PILLOW.get());
-                    })
-                    .build());
-
+    public static final RegistryObject<CreativeModeTab> HB_TAB =
+        CREATIVE_MODE_TABS.register("hb_tab", () ->
+            CreativeModeTab.builder()
+                .icon(() -> new ItemStack(ModItems.PILLOW.get()))
+                .title(Component.translatable("creativetab.hb_tab"))
+                .displayItems((pParameters, pOutput) -> {
+                    pOutput.accept(ModItems.EXAMPLE_ITEM.get());
+                    pOutput.accept(ModItems.PILLOW.get());
+                    pOutput.accept(ModItems.SEWING_KIT.get());
+                    pOutput.accept(ModItems.CLOTH.get());
+                    pOutput.accept(ModItems.CLOTH_KNIT.get());
+                    pOutput.accept(ModItems.STRING_KNIT.get());
+                    pOutput.accept(ModItems.KNITTING_STICKS.get());
+                    pOutput.accept(ModItems.FLINT_SHEARS.get());
+                    pOutput.accept(ModItems.WHITE_SHEET.get());
+                    pOutput.accept(ModItems.BLUE_SHEET.get());
+                    pOutput.accept(ModItems.GREEN_SHEET.get());
+                    pOutput.accept(ModItems.RED_SHEET.get());
+                    pOutput.accept(ModItems.YELLOW_SHEET.get());
+                    pOutput.accept(ModItems.BLACK_SHEET.get());
+                    pOutput.accept(ModItems.ORANGE_SHEET.get());
+                    pOutput.accept(ModItems.PURPLE_SHEET.get());
+                    pOutput.accept(ModItems.BROWN_SHEET.get());
+                    pOutput.accept(ModItems.GRAY_SHEET.get());
+                    pOutput.accept(ModItems.MAGENTA_SHEET.get());
+                    pOutput.accept(ModItems.CYAN_SHEET.get());
+                    pOutput.accept(ModItems.LIME_SHEET.get());
+                    pOutput.accept(ModItems.PINK_SHEET.get());
+                    pOutput.accept(ModItems.LIGHT_BLUE_SHEET.get());
+                    pOutput.accept(ModItems.LIGHT_GRAY_SHEET.get());
+                })
+                .build()
+        );
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
